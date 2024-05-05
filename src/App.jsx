7087e74808cc6e6d940e926d4dd5f2c930194120
Jaonaaa <jaonapaulferdinah@gmail.com>
2024-05-05 08:15:20 +0300

    vao zao

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { AnimatePresence } from "framer-motion";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/home";
-import ClientNav from "./components/navigation/client-nav";
+import ClientNav from "./components/navigation/client-navigation";
 import ClientNavigation from "./components/navigation/client-navigation";
 
 function App() {
@@ -9,38 +9,15 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-      {/* <Navigation /> */}
-=======
       <Navigation />
->>>>>>> b8e32c2 (mety)
-      {/* <SOS /> */}
-      {/* <Cursor /> */}
-      {/* <ClientNav /> */}
+
       <ClientNavigation />
 
-      {/* <Sign /> */}
-=======
-      <Navigation />
-      {/* <SOS /> */}
-      {/* <Cursor /> */}
-      {/* <ClientNav /> */}
-      {/* <ClientNavigation /> */}
->>>>>>> 985787d (Hello world)
-=======
-      {/* <Navigation /> */}
-      {/* <SOS /> */}
-      {/* <Cursor /> */}
-      {/* <ClientNav /> */}
       <ClientNavigation />
->>>>>>> ebda72d (Hello)
       <AnimatePresence initial={false} mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route element={<Home />} path="/" />
           <Route element={<></>} path="/agent" />
->>>>>>> 985787d (Hello world)
         </Routes>
       </AnimatePresence>
     </>
