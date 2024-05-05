0d324340d4f95cf0b1d360bc6e44badc5d8bd708
Jaonaaa <jaonapaulferdinah@gmail.com>
2024-05-05 04:45:32 +0300

    com

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,32 @@
 import { AnimatePresence } from "framer-motion";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/home";
-<<<<<<< HEAD
 import Navigation from "./components/navigation";
 import SOS from "./components/sos";
 import Gadgets from "./pages/gadgets";
 import Sign from "./components/Sign/Sign";
 import Archive from "./pages/archive";
 import Profile from "./pages/profile";
-import PlaneViewer from "./components/PlaneViewer";
 // import Cursor from "./components/cursor";
-=======
-import Gadgets from "./pages/gadgets";
-import ClientNav from "./components/navigation/client-nav";
-import ClientNavigation from "./components/navigation/client-navigation";
-import Navigation from "./components/navigation/";
->>>>>>> 985787d (Hello world)
 
 function App() {
   const location = useLocation();
 
   return (
     <>
-<<<<<<< HEAD
-      <PlaneViewer />
-
-      <Navigation />
-      {/* <SOS /> */}
-      {/* <Cursor /> */}
-      {/* <Sign /> */}
-=======
-      <Navigation />
+      {/* <Navigation /> */}
       {/* <SOS /> */}
       {/* <Cursor /> */}
       {/* <ClientNav /> */}
-      {/* <ClientNavigation /> */}
->>>>>>> 985787d (Hello world)
+      <ClientNavigation />
+
+      {/* <Sign /> */}
       <AnimatePresence initial={false} mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route element={<Home />} path="/" />
           <Route element={<Gadgets />} path="/gadgets" />
-<<<<<<< HEAD
           <Route element={<Archive />} path="/archives" />
           <Route element={<Profile />} path="/profile" />
-=======
-          <Route element={<></>} path="/agent" />
->>>>>>> 985787d (Hello world)
         </Routes>
       </AnimatePresence>
     </>
